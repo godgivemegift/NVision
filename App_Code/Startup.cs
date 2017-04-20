8f90c20e1bccb61551da3db08bc3cb6337ac1469
xun.a.cao <xun.a.cao@accenture.com>
2017-04-20 15:30:08 +0200

    first commit

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NVision.Startup))]
+namespace NVision
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
